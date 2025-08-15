25377927174e86b57c34c5d6e0f1c1cbad4c3e18
Your Name <you@example.com>
2025-08-15 09:11:38 -0600

    login signup

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,20 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/Cards/cards_data.js'
 
 
 const TitleCards = ({title, category}) => {
+
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef(null);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNDVmOGM3ZGI3NjU3MGI0Mzc3OWMzODQwYjRkMmU3YSIsIm5iZiI6MTc1MzY2MTY1Mi4wMzEwMDAxLCJzdWIiOiI2ODg2YzBkNDYzMDkyMjJmZWU3MmVmNjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.QQRVmExWaXGMHoX2-ZHMEZT8bLagHvSn1SqXNGS5GU8'
+    }
+  };
 
   const handleWheel = (e) => {
     e.preventDefault();
@@ -12,6 +22,11 @@ const TitleCards = ({title, category}) => {
   };
 
   useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => setApiData(response.results))
+    .catch(err => console.error(err));
+
     const el = cardsRef.current;
     if (!el) return;
 
