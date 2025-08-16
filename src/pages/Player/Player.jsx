ee1739f8c20eab7ea4f558043d12df2c7cd937c3
Your Name <you@example.com>
2025-08-16 10:12:07 -0600

    nav scroll

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function Player() {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const[apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  })
+
+    const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNDVmOGM3ZGI3NjU3MGI0Mzc3OWMzODQwYjRkMmU3YSIsIm5iZiI6MTc1MzY2MTY1Mi4wMzEwMDAxLCJzdWIiOiI2ODg2YzBkNDYzMDkyMjJmZWU3MmVmNjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.QQRVmExWaXGMHoX2-ZHMEZT8bLagHvSn1SqXNGS5GU8'
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+  }, [])
+  
   return (
     <div className='player'>
-      <img src={back_arrow_icon} alt="" />
+      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}} />
       <iframe width='90%' height='90%'
-      src='https://www.youtube.com/embed/'></iframe>
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer'frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
